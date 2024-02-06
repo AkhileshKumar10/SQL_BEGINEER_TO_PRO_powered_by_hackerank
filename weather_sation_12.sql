50c220fa9620e9782be7f4246d731f02361b6b60
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-06 23:29:13 +0530

    Add SQL query to filter out cities starting and ending with vowels

---
+++ weather_sation_12.sql
@@ -0,0 +1,28 @@
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+
+-- select distinct city 
+-- from station 
+--  WHERE LOWER(CITY) NOT LIKE 'a%a' 
+--    AND LOWER(CITY) NOT  LIKE 'e%e' 
+--    AND LOWER(CITY) NOT  LIKE 'i%i' 
+--    AND LOWER(CITY) NOT  LIKE 'o%o' 
+--    AND LOWER(CITY) NOT LIKE 'u%u';
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(CITY) NOT LIKE 'a%'
+  AND LOWER(CITY) NOT LIKE 'e%'
+  AND LOWER(CITY) NOT LIKE 'i%'
+  AND LOWER(CITY) NOT LIKE 'o%'
+  AND LOWER(CITY) NOT LIKE 'u%'
+  AND LOWER(CITY) NOT LIKE '%a'
+  AND LOWER(CITY) NOT LIKE '%e'
+  AND LOWER(CITY) NOT LIKE '%i'
+  AND LOWER(CITY) NOT LIKE '%o'
+  AND LOWER(CITY) NOT LIKE '%u';
