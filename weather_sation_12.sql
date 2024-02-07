085bf018b002353e5417ebc53fac490881f3d53e
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-07 22:34:20 +0530

    Refactor city filtering in weather_sation_12.sql

--- weather_sation_12.sql
+++ weather_sation_12.sql
@@ -14,15 +14,40 @@
 --    AND LOWER(CITY) NOT  LIKE 'o%o' 
 --    AND LOWER(CITY) NOT LIKE 'u%u';
 
-SELECT DISTINCT CITY
-FROM STATION
-WHERE LOWER(CITY) NOT LIKE 'a%'
-  AND LOWER(CITY) NOT LIKE 'e%'
-  AND LOWER(CITY) NOT LIKE 'i%'
-  AND LOWER(CITY) NOT LIKE 'o%'
-  AND LOWER(CITY) NOT LIKE 'u%'
-  AND LOWER(CITY) NOT LIKE '%a'
-  AND LOWER(CITY) NOT LIKE '%e'
-  AND LOWER(CITY) NOT LIKE '%i'
-  AND LOWER(CITY) NOT LIKE '%o'
-  AND LOWER(CITY) NOT LIKE '%u';
+-- SELECT DISTINCT CITY
+-- FROM STATION
+-- WHERE LOWER(CITY) NOT LIKE 'a%'
+--   AND LOWER(CITY) NOT LIKE 'e%'
+--   AND LOWER(CITY) NOT LIKE 'i%'
+--   AND LOWER(CITY) NOT LIKE 'o%'
+--   AND LOWER(CITY) NOT LIKE 'u%'
+--   AND LOWER(CITY) NOT LIKE '%a'
+--   AND LOWER(CITY) NOT LIKE '%e'
+--   AND LOWER(CITY) NOT LIKE '%i'
+--   AND LOWER(CITY) NOT LIKE '%o'
+--   AND LOWER(CITY) NOT LIKE '%u';
+-- SELECT DISTINCT city 
+-- FROM station 
+-- WHERE city NOT LIKE '%a' 
+--   AND city NOT LIKE '%e' 
+--   AND city NOT LIKE '%i' 
+--   AND city NOT LIKE '%o' 
+--   AND city NOT LIKE '%u' 
+--   AND city NOT LIKE 'a%' 
+--   AND city NOT LIKE 'e%' 
+--   AND city NOT LIKE 'i%' 
+--   AND city NOT LIKE 'o%' 
+--   AND city NOT LIKE 'u%';
+SELECT DISTINCT city 
+FROM station 
+WHERE city NOT LIKE 'A%' 
+  AND city NOT LIKE 'E%' 
+  AND city NOT LIKE 'I%' 
+  AND city NOT LIKE 'O%' 
+  AND city NOT LIKE 'U%' 
+  AND city NOT LIKE '%a' 
+  AND city NOT LIKE '%e' 
+  AND city NOT LIKE '%i' 
+  AND city NOT LIKE '%o' 
+  AND city NOT LIKE '%u';
+
